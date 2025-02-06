6f83628cc3121dc54608a31e22868dc25282b7c9
Jukka Malinen <Jukka.malinen@laurea.fi>
2025-02-06 08:56:04 +0200

    update

--- Lecture_ex/javascript.js
+++ Lecture_ex/javascript.js
@@ -3,3 +3,21 @@ function myFirstFunction(number) {
 
     return 2 * number;
 }
+
+function myFunction(){
+    var x = 5;
+    var y = 6;
+    var z = x + y;
+    document.getElementById("demo").innerHTML = z;
+}
+
+function showXY(event){
+    var x = event.clientX;
+    var y = event.clientY;
+    var coords = "X coords: " + x + ",<br> Y coords: " + y;
+    document.getElementById("coordinates").innerHTML = coords;
+}   
+
+function buttonFunction(){
+    alert("Button clicked");
+}   
